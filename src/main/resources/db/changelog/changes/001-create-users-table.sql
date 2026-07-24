be436e62fedf18ea14642729f63870903e41918d
hassanmallah <hassanmallah02@gmail.com>
2026-07-24 14:17:46 +0500

    convert id type from BIGSERIAL to UUID in 001-create-users-table.sql file

--- src/main/resources/db/changelog/changes/001-create-users-table.sql
+++ src/main/resources/db/changelog/changes/001-create-users-table.sql
@@ -1,6 +1,6 @@
 --changeset hassan:001
 CREATE TABLE users (
-                       id BIGSERIAL PRIMARY KEY,
+                       id  UUID PRIMARY KEY      DEFAULT gen_random_uuid(),
                        name VARCHAR(100) NOT NULL,
                        email VARCHAR(255) NOT NULL UNIQUE,
                        password VARCHAR(255),
